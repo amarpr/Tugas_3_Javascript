d2a8f3c3ced1ef21a7fb65948845983106b3c0f9
Muhamar <amararrasyid@gmail.com>
2020-05-03 12:50:51 +0700

    Tugas 3 Javascript
    Mengumpulkan TUgas Niomic

---
+++ index.js
@@ -0,0 +1,20 @@
+// Memiliki return
+
+function cinta() {
+    var aku = "Sayang Kamu"
+    return aku;
+}
+
+var kita = cinta();
+console.log(kita)
+
+
+
+// Memiliki parameter
+function nama(depan, belakang) {
+
+    console.log(depan);
+    console.log(belakang);
+}
+
+nama("Muhamar", "Ar-Rasyid");
